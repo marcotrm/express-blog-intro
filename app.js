920244fd60045591e42b808f39d2905a6e1c46ac
marco torremacco <mtorremacco@gmail.comm>
2025-01-20 14:16:13 +0100

    feat add 'public'

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
@@ -42,6 +44,7 @@ app.get("/bacheca", (req, res) => {
           ]
     )
 })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
